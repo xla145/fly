d125b80e4fab867dbe18e68c71f4995725855b92
xula <xula@yuelinghui.com>
2018-11-29 12:03:55 +0800

    1:修改数据库信息

--- document/fly.sql
+++ document/fly.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50723
 File Encoding         : 65001
 
-Date: 2018-11-06 18:40:24
+Date: 2018-11-29 12:02:48
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,7 +20,7 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `article`;
 CREATE TABLE `article` (
-  `id` int(11) NOT NULL,
+  `aid` varchar(30) NOT NULL COMMENT '文章编号',
   `title` varchar(255) NOT NULL COMMENT '文章标题',
   `cat_id` int(11) NOT NULL COMMENT '文章分类',
   `cat_name` varchar(50) NOT NULL COMMENT '分类名称',
@@ -32,11 +32,13 @@ CREATE TABLE `article` (
   `pay_point` int(11) NOT NULL DEFAULT '10' COMMENT '发布文章消耗积分 10 - 200 之间（后台字典配置）',
   `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态  -1：已删除 0：待审核 5：成功发布 10：未结 15：完结 20：不通过',
   `remark` varchar(255) DEFAULT NULL COMMENT '备注',
-  `label` varchar(100) NOT NULL COMMENT '标签',
-  `weight` tinyint(4) NOT NULL COMMENT '权重',
+  `label` varchar(100) DEFAULT NULL COMMENT '标签',
+  `weight` tinyint(4) NOT NULL DEFAULT '0' COMMENT '权重',
   `is_top` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否置顶',
-  `is_comment` tinyint(4) NOT NULL COMMENT '是否支持评论',
-  PRIMARY KEY (`id`)
+  `is_comment` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否支持评论',
+  `comment_num` int(11) NOT NULL DEFAULT '0' COMMENT '评论数',
+  `is_good` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否为精贴',
+  PRIMARY KEY (`aid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -56,14 +58,16 @@ CREATE TABLE `article_browse` (
 -- ----------------------------
 DROP TABLE IF EXISTS `article_category`;
 CREATE TABLE `article_category` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL COMMENT '分类名称',
+  `alias` varchar(100) NOT NULL COMMENT '别名',
   `status` tinyint(4) NOT NULL COMMENT '状态 0：停用 1：启用',
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  `is_hot` tinyint(255) NOT NULL DEFAULT '0' COMMENT '是否是热门分类',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for article_comment
@@ -77,6 +81,17 @@ CREATE TABLE `article_comment` (
   `create_time` datetime NOT NULL COMMENT '创建时间',
   `remark` varchar(255) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for article_comment_love
+-- ----------------------------
+DROP TABLE IF EXISTS `article_comment_love`;
+CREATE TABLE `article_comment_love` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `ac_id` int(11) NOT NULL,
+  `create_time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -95,17 +110,6 @@ CREATE TABLE `article_comment_replay` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for article_commont_love
--- ----------------------------
-DROP TABLE IF EXISTS `article_commont_love`;
-CREATE TABLE `article_commont_love` (
-  `id` int(11) NOT NULL,
-  `uid` int(11) NOT NULL,
-  `ac_id` int(11) NOT NULL,
-  `create_time` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
 -- Table structure for dict_group
 -- ----------------------------
 DROP TABLE IF EXISTS `dict_group`;
@@ -118,7 +122,7 @@ CREATE TABLE `dict_group` (
   `update_time` datetime NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `index_dict_group_code` (`code`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COMMENT='字典组';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='字典组';
 
 -- ----------------------------
 -- Table structure for dict_item
@@ -134,7 +138,7 @@ CREATE TABLE `dict_item` (
   `info` varchar(512) DEFAULT NULL COMMENT '备注信息',
   PRIMARY KEY (`id`),
   UNIQUE KEY `ids_1` (`group_code`,`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8 COMMENT='字典项';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='字典项';
 
 -- ----------------------------
 -- Table structure for image_text_group
@@ -150,7 +154,7 @@ CREATE TABLE `image_text_group` (
   `status` int(5) NOT NULL DEFAULT '0' COMMENT '状态 1：启动 0：停用',
   PRIMARY KEY (`id`),
   UNIQUE KEY `index_data_dic_group_code` (`code`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for image_text_item
@@ -168,7 +172,7 @@ CREATE TABLE `image_text_item` (
   `update_time` datetime NOT NULL,
   `remark` varchar(512) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=136 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for member
@@ -176,7 +180,6 @@ CREATE TABLE `image_text_item` (
 DROP TABLE IF EXISTS `member`;
 CREATE TABLE `member` (
   `uid` int(11) NOT NULL AUTO_INCREMENT COMMENT 'uid',
-  `status` int(5) NOT NULL DEFAULT '1' COMMENT '状态：0 无效， 1 有效， -1 删除',
   `password` varchar(128) NOT NULL COMMENT '登录密码',
   `nickname` varchar(128) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '昵称',
   `avatar` varchar(256) DEFAULT NULL COMMENT '头像',
@@ -186,14 +189,16 @@ CREATE TABLE `member` (
   `ip` varchar(64) DEFAULT NULL COMMENT '注册ip',
   `create_time` datetime NOT NULL COMMENT '注册时间',
   `update_time` datetime NOT NULL COMMENT '更新时间',
+  `status` int(5) NOT NULL DEFAULT '1' COMMENT '状态：0 无效，2：用户未激活邮箱  5：有效， -1 删除',
   `last_time` datetime NOT NULL COMMENT '最后登录时间',
   `city` varchar(20) DEFAULT NULL COMMENT '城市',
   `birthday` datetime DEFAULT NULL COMMENT '会员生日',
   `channel_id` int(11) NOT NULL DEFAULT '1' COMMENT '注册渠道编号 默认1 自注册 2:qq 3:微博',
-  `email` varchar(100) NOT NULL COMMENT '邮箱',
+  `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
   `remark` varchar(128) DEFAULT NULL COMMENT '备注',
+  `signature` varchar(255) NOT NULL DEFAULT '' COMMENT '签名',
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=16368 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for member_article
@@ -226,7 +231,7 @@ CREATE TABLE `member_grow_log` (
   `type` tinyint(4) NOT NULL COMMENT '来源类型 1：签到 2：发表文章 3：后台赠送 4：其他',
   PRIMARY KEY (`id`),
   KEY `ids_1` (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2446 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2454 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for member_info
@@ -237,7 +242,7 @@ CREATE TABLE `member_info` (
   `vip` int(3) NOT NULL DEFAULT '1' COMMENT '会员当前vip等级',
   `vip_name` varchar(256) NOT NULL DEFAULT '普通会员' COMMENT '会员当前vip等级名',
   `growth_value` bigint(20) NOT NULL DEFAULT '0' COMMENT '会员当前成长值',
-  `point_value` bigint(20) NOT NULL DEFAULT '0' COMMENT '会员当前积分',
+  `point_value` bigint(20) DEFAULT '0' COMMENT '会员当前积分',
   `create_time` datetime NOT NULL COMMENT '创建时间',
   `update_time` datetime NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`uid`)
@@ -279,11 +284,12 @@ CREATE TABLE `member_message` (
 DROP TABLE IF EXISTS `member_qq`;
 CREATE TABLE `member_qq` (
   `uid` int(11) NOT NULL COMMENT '用户uid',
-  `open_id` varchar(200) DEFAULT NULL COMMENT '用户openID',
   `create_time` datetime NOT NULL COMMENT '创建时间',
   `remark` varchar(256) DEFAULT NULL COMMENT '备注',
-  PRIMARY KEY (`uid`),
-  KEY `uniq_1` (`open_id`) USING BTREE
+  `uuid` varchar(100) NOT NULL COMMENT '用户openID',
+  `update_time` datetime NOT NULL COMMENT '授权状态 1：绑定中 2：解绑',
+  `status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '授权状态 1：绑定中 2：解绑',
+  PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -292,13 +298,83 @@ CREATE TABLE `member_qq` (
 DROP TABLE IF EXISTS `member_weibo`;
 CREATE TABLE `member_weibo` (
   `uid` int(11) NOT NULL COMMENT '用户uid',
-  `open_id` varchar(200) DEFAULT NULL COMMENT '用户openID',
   `create_time` datetime NOT NULL COMMENT '创建时间',
   `remark` varchar(256) DEFAULT NULL COMMENT '备注',
-  `xcx_open_id` varchar(200) DEFAULT NULL COMMENT '小程序用户openid',
+  `uuid` varchar(200) NOT NULL COMMENT '微博的用户uid',
+  `update_time` datetime NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '授权状态 1：绑定中 2：解绑',
   PRIMARY KEY (`uid`),
-  KEY `uniq_1` (`open_id`) USING BTREE,
-  KEY `xcx_open_id` (`xcx_open_id`) USING BTREE
+  KEY `xcx_open_id` (`uuid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for sms_conf
+-- ----------------------------
+DROP TABLE IF EXISTS `sms_conf`;
+CREATE TABLE `sms_conf` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `cmd` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '标记',
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '状态  0：初始状态 10：启用 15：停用',
+  `plan_class` varchar(512) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '类名',
+  `plan_describe` varchar(512) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '描述',
+  `temp` varchar(64) NOT NULL COMMENT '模板',
+  `aisle` varchar(64) NOT NULL DEFAULT '' COMMENT '通道',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '修改时间',
+  `remark` varchar(256) DEFAULT '' COMMENT '备注',
+  `temp_content` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cmd` (`cmd`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for sms_oper
+-- ----------------------------
+DROP TABLE IF EXISTS `sms_oper`;
+CREATE TABLE `sms_oper` (
+  `sid` int(11) NOT NULL COMMENT '主键',
+  `aclass` varchar(512) DEFAULT NULL COMMENT '操作类com.xx.oo',
+  `describe` varchar(255) DEFAULT NULL COMMENT '描述',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for sms_record
+-- ----------------------------
+DROP TABLE IF EXISTS `sms_record`;
+CREATE TABLE `sms_record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `type` int(5) NOT NULL COMMENT '类型 1： 验证码类  2：通知类',
+  `mobile` varchar(20) NOT NULL COMMENT '手机号',
+  `vcode` varchar(20) NOT NULL COMMENT '验证码',
+  `status` int(5) NOT NULL DEFAULT '0' COMMENT '0 未使用（未验证） 1：已使用（已验证） 2：已过期',
+  `temp_id` varchar(64) NOT NULL COMMENT '短信模板id',
+  `temp_content` varchar(512) DEFAULT NULL COMMENT '短信模板内容',
+  `provider` varchar(64) NOT NULL COMMENT '提供商',
+  `send_time` datetime NOT NULL COMMENT '发送时间',
+  `use_time` datetime DEFAULT NULL COMMENT '使用 时间',
+  `expire_time` datetime NOT NULL COMMENT '有效期',
+  `remark` varchar(256) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`),
+  KEY `ids_1` (`mobile`,`temp_id`),
+  KEY `ids_2` (`status`),
+  KEY `ids_3` (`expire_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for sms_temp
+-- ----------------------------
+DROP TABLE IF EXISTS `sms_temp`;
+CREATE TABLE `sms_temp` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '模板编号',
+  `title` varchar(100) NOT NULL COMMENT '模板标题',
+  `content` varchar(255) NOT NULL COMMENT '模板内容',
+  `create_time` datetime NOT NULL,
+  `upadate_time` datetime NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '模板状态 0：停用 1:启用 ',
+  `remark` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -430,3 +506,50 @@ CREATE TABLE `sys_user_role` (
   `remark` varchar(200) NOT NULL COMMENT '备注',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for vip_grade
+-- ----------------------------
+DROP TABLE IF EXISTS `vip_grade`;
+CREATE TABLE `vip_grade` (
+  `vid` int(11) NOT NULL AUTO_INCREMENT,
+  `vip` int(3) NOT NULL COMMENT 'vip等级',
+  `name` varchar(64) NOT NULL COMMENT 'vip等级名',
+  `info` varchar(256) DEFAULT NULL COMMENT 'vip描述',
+  `icon` varchar(256) DEFAULT NULL COMMENT 'vip icon',
+  `growth_value` bigint(20) NOT NULL DEFAULT '0' COMMENT '成长值量',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `remark` varchar(256) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`vid`),
+  KEY `vip` (`vip`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for vip_grade_task
+-- ----------------------------
+DROP TABLE IF EXISTS `vip_grade_task`;
+CREATE TABLE `vip_grade_task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `vip` int(11) NOT NULL COMMENT 'vip等级',
+  `vtid` int(11) NOT NULL COMMENT 'vip等级任务id',
+  `param` varchar(256) DEFAULT NULL COMMENT '参数',
+  `remark` varchar(256) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `vip` (`vip`,`vtid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for vip_task
+-- ----------------------------
+DROP TABLE IF EXISTS `vip_task`;
+CREATE TABLE `vip_task` (
+  `vtid` int(11) NOT NULL AUTO_INCREMENT COMMENT ' vip升级任务id',
+  `name` varchar(64) NOT NULL COMMENT 'vip升级任务名',
+  `info` varchar(500) DEFAULT NULL COMMENT 'vip升级任务描述',
+  `clazz` varchar(256) NOT NULL COMMENT '可运行类名',
+  `param_dec` varchar(512) NOT NULL COMMENT '参数说明',
+  `status` int(5) NOT NULL DEFAULT '0' COMMENT '状态 0 无效 1 有效',
+  `create_time` datetime NOT NULL,
+  `remark` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`vtid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
